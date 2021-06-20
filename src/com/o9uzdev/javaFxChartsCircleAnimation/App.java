c6f23406b184264e717a9504de09bbb43509c55b
o9uzdev <o9uzdev@gmail.com>
2021-06-20 20:41:59 +0300

    Added keypress events

--- src/com/o9uzdev/javaFxChartsCircleAnimation/App.java
+++ src/com/o9uzdev/javaFxChartsCircleAnimation/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Pane pane = new Pane();
-        Circle circle = new Circle(100,100,50);
+        Circle circle = new Circle(100, 100, 50);
         circle.setFill(Color.BLUEVIOLET);
         circle.setFocusTraversable(true);
         //when focusTraversable property is true focus can be moved to circle
@@ -22,9 +22,32 @@ public class App extends Application {
             circle.setCenterY(e.getY());
         });
 
+        circle.setOnMouseClicked(event -> circle.requestFocus());
+        circle.setOnMouseDragged(e -> {
+            circle.setCenterX(e.getX());
+            circle.setCenterY(e.getY());
+        });
+
+        circle.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case DOWN:
+                    circle.setCenterY(circle.getCenterY() + 10);
+                    break;
+                case UP:
+                    circle.setCenterY(circle.getCenterY() - 10);
+                    break;
+                case LEFT:
+                    circle.setCenterX(circle.getCenterX() - 10);
+                    break;
+                case RIGHT:
+                    circle.setCenterX(circle.getCenterX() + 10);
+                    break;
+            }
+        });
+
         pane.getChildren().addAll(circle);
 
-        stage.setScene(new Scene(pane,640,480));
+        stage.setScene(new Scene(pane, 640, 480));
         stage.setTitle("JavaFx Charts Circle Animation");
         stage.show();
     }
